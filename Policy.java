1f859a789a808020550857a35d9d2196d700f6c7
cout-ana-jackson <anathejedi@gmail.com>
2025-04-14 17:47:55 -0400

    Update Policy.java
    Moved fields and methods to the PolicyHolder class

--- Policy.java
+++ Policy.java
@@ -1,150 +1,58 @@
-import java.util.Scanner;
-
-/**
- * Represents an insurance policy with details.
- */
 public class Policy {
-    // Data members
     private String policyNumber;
     private String providerName;
-    private String policyHolderFirstName;
-    private String policyHolderLastName;
-    private int policyHolderAge;
-    private String policyHolderSmokingStatus;
-    private double policyHolderHeight;
-    private double policyHolderWeight;
+    private PolicyHolder policyHolder;
 
-    /**
-     * Default constructor initializing policy details to default values.
-     */
+    private static int policyCount = 0;
+
+    // Default constructor
     public Policy() {
         this.policyNumber = "";
         this.providerName = "";
-        this.policyHolderFirstName = "";
-        this.policyHolderLastName = "";
-        this.policyHolderAge = 0;
-        this.policyHolderSmokingStatus = "non-smoker";
-        this.policyHolderHeight = 0.0;
-        this.policyHolderWeight = 0.0;
+        this.policyHolder = new PolicyHolder();
+        policyCount++;
     }
 
-    /**
-     * Constructor initializing policy details with provided values.
-     *
-     * @param policyNumber The policy number.
-     * @param providerName The name of the insurance provider.
-     * @param policyHolderFirstName The first name of the policyholder
-     * @param policyHolderLastName The last name of the policyholder
-     * @param policyHolderAge age of the policyholder
-     * @param policyHolderSmokingStatus The smoking status of the policyholder 
-     * @param policyHolderHeight height of the policyholder in inches.
-     * @param policyHolderWeight weight of the policyholder in pounds.
-     */
-    public Policy(String policyNumber, String providerName, String policyHolderFirstName,
-                  String policyHolderLastName, int policyHolderAge, String policyHolderSmokingStatus,
-                  double policyHolderHeight, double policyHolderWeight) {
+    // Full constructor with deep copy
+    public Policy(String policyNumber, String providerName, PolicyHolder policyHolder) {
         this.policyNumber = policyNumber;
         this.providerName = providerName;
-        this.policyHolderFirstName = policyHolderFirstName;
-        this.policyHolderLastName = policyHolderLastName;
-        this.policyHolderAge = policyHolderAge;
-        this.policyHolderSmokingStatus = policyHolderSmokingStatus;
-        this.policyHolderHeight = policyHolderHeight;
-        this.policyHolderWeight = policyHolderWeight;
+        this.policyHolder = new PolicyHolder(policyHolder);
+        policyCount++;
     }
 
-    /**
-     * Gets the policy number.
-     *
-     * @return The policy number.
-     */
     public String getPolicyNumber() {
         return policyNumber;
     }
 
-    /**
-     * Gets the provider name.
-     *
-     * @return The insurance provider name.
-     */
     public String getProviderName() {
         return providerName;
     }
 
-    /**
-     * Gets the first name of the policyholder.
-     *
-     * @return The first name of the policyholder.
-     */
-    public String getPolicyHolderFirstName() {
-        return policyHolderFirstName;
+    public PolicyHolder getPolicyHolder() {
+        return new PolicyHolder(policyHolder); // deep copy return
     }
 
-    /**
-     * Gets the last name of the policyholder.
-     *
-     * @return The last name of the policyholder.
-     */
-    public String getPolicyHolderLastName() {
-        return policyHolderLastName;
-    }
-
-    /**
-     * Gets the age of the policyholder.
-     *
-     * @return The age of the policyholder.
-     */
-    public int getPolicyHolderAge() {
-        return policyHolderAge;
-    }
-
-    /**
-     * Gets the smoking status of the policyholder.
-     *
-     * @return The smoking status ("smoker" or "non-smoker").
-     */
-    public String getPolicyHolderSmokingStatus() {
-        return policyHolderSmokingStatus;
-    }
-
-    /**
-     * Gets the height of the policyholder.
-     *
-     * @return The height of the policyholder in inches.
-     */
-    public double getPolicyHolderHeight() {
-        return policyHolderHeight;
-    }
-
-    /**
-     * Gets the weight of the policyholder.
-     *
-     * @return The weight of the policyholder in pounds.
-     */
-    public double getPolicyHolderWeight() {
-        return policyHolderWeight;
-    }
-
-    /**
-     * Calculates the bmi of the policyholder
-     *
-     * @return The BMI value calculated using weight and height.
-     */
-    public double calculateBMI() {
-        return (policyHolderWeight * 703) / (policyHolderHeight * policyHolderHeight);
-    }
-
-    /**
-     * Calculates the insurance policy price based on policyholder's attributes.
-     *
-     * @return The calculated policy price.
-     */
     public double calculatePolicyPrice() {
         double baseFee = 600;
-        if (policyHolderAge > 50) baseFee += 75;
-        if (policyHolderSmokingStatus.equalsIgnoreCase("smoker")) baseFee += 100;
-        double bmi = calculateBMI();
+        if (policyHolder.getAge() > 50) baseFee += 75;
+        if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) baseFee += 100;
+        double bmi = policyHolder.calculateBMI();
         if (bmi > 35) baseFee += (bmi - 35) * 20;
         return baseFee;
+    }
+
+    public static int getPolicyCount() {
+        return policyCount;
+    }
+
+    public String toString() {
+        return String.format(
+            "Policy Number: %s\nProvider Name: %s\n%s\nPolicy Price: $%.2f",
+            policyNumber,
+            providerName,
+            policyHolder.toString(),
+            calculatePolicyPrice()
+        );
     }
 }
